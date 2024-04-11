e127368ac645a9abbc0af4a25cc2424cac86f795
yeonyyy <layoun9108@gmail.com>
2024-04-11 21:56:40 +0900

    modify

--- PinterestSample/Model/Photo.swift
+++ PinterestSample/Model/Photo.swift
@@ -12,18 +12,18 @@ struct Photo {
     var title : String
     
     static func getPhotos() -> [Photo] {
-        let photos = [Photo(name: "image0", title: "*djkdjkd*"),
-                      Photo(name: "image1", title: "*djkdjkdjkjkjkjkjk*"),
-                      Photo(name: "image2", title: "*ccc*"),
-                      Photo(name: "image3", title: "*ddddddddddddddddddddddddddddddddddddjkdljldjldjldddddd*"),
-                      Photo(name: "image4", title: "*aaaaaaaaa*"),
-                      Photo(name: "image5", title: "*a*"),
-                      Photo(name: "image6", title: "*djkd*"),
-                      Photo(name: "image7", title: "*b*"),
-                      Photo(name: "image8", title: "*aaaaaaaaa*"),
-                      Photo(name: "image9", title: "*akjlalajlajlajaljaljalajlajakdjkjkbjkbjkbjbkbjbjk*"),
-                      Photo(name: "image10", title: "*aaaaaaaaa*"),
-                      Photo(name: "image11", title: "*bbjkjbkjbkjbkjb*")]
+        let photos = [Photo(name: "image0", title: "Birds are attractive creatures that have various behaviors and adaptations."),
+                      Photo(name: "image1", title: "Here's How Watermelon Can Help Control Blood Pressure This Summer"),
+                      Photo(name: "image2", title: "He is an African grey parrot"),
+                      Photo(name: "image3", title: "tulip"),
+                      Photo(name: "image4", title: "night"),
+                      Photo(name: "image5", title: "The Old Man and the Sea"),
+                      Photo(name: "image6", title: "car"),
+                      Photo(name: "image7", title: "the lights of the city"),
+                      Photo(name: "image8", title: "The Dolphin and the Baby"),
+                      Photo(name: "image9", title: "Disneyland, a fairy tale land"),
+                      Photo(name: "image10", title: "green parrot"),
+                      Photo(name: "image11", title: "Santorini")]
         return photos
     }
 }
